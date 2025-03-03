39cc881792550d04948bd4e081b156b1160789c1
gaurav-1024 <kumar2503gaurav@gmail.com>
2025-03-03 15:13:47 +0530

    enhance

--- views/js/searchbooks.js
+++ views/js/searchbooks.js
@@ -5,4 +5,10 @@ btn.addEventListener("click",async ()=>{
     let response=await fetch(`/fetchbooks/${val}`)
     let data=await response.json()
     // console.log(data[0].AuthorName)
+    let cont=document.getElementsByClassName("uploadSection")[0]
+    cont.innerHTML=""
+    data.forEach((elem)=>{
+        cont.innerHTML+=`<div>AuthorName ${elem.AuthorName}<br>BookName ${elem.BookName}<br></div>`
+    })
 })
+
